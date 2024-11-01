a4f8428cd37c9481909cc05f92d0c57253712bc1
rafa <r7fael@hotmail.com>
2024-11-01 13:13:46 -0300

    commit_2

--- crud_training.py
+++ crud_training.py
@@ -1,5 +1,31 @@
+import os
+os.system("cls")
+
 workouts = []
 
+def save_workout_data(filename="workout_data.txt"):
+    with open("workout_data.txt", "w") as file:
+        for workout in workouts:
+            line = f"{workout['id']},{workout['name']},{workout['date']},{workout['distance']},{workout['time']},{workout['localization']},{workout['weather']}\n"
+            file.write(line)
+
+def read_workout_data(filename="workout_data.txt"):
+    global workouts
+    workouts = []
+    with open("workout_data.txt", "r") as file:
+        for line in file:
+            id, name, date, distance, time, localization, weather = line.strip().split(',')
+            workout = {
+                    "id": int(id),
+                    "name": name,
+                    "date": date,
+                    "distance": float(distance),
+                    "time": float(time),
+                    "localization": localization,
+                    "weather": weather
+                }
+            workouts.append(workout)
+            
 def create_workout(name, date, distance, time, localization, weather):
     new_workout = {
         "id": len(workouts) + 1,
@@ -12,15 +38,16 @@ def create_workout(name, date, distance, time, localization, weather):
     } 
     workouts.append(new_workout)
     print("Treino criado com sucesso!")
+    save_workout_data()
 
 def read_workout():
     if not workouts:
         print("Sem treinos registrados!")
     for i, workout in enumerate(workouts):
-        print(f"Treino {i + 1}:\n Nome: {workout["name"]} \n Data: {workout["date"]} \n Distância: {workout["distance"]}KM \n Tempo: {workout["time"]} \n Localização: {workout["localization"]} \n Clima: {workout["weather"]}")
+        print(f"Treino {i + 1}:\n  Nome: {workout["name"]} \n  Data: {workout["date"]} \n  Distância: {workout["distance"]}KM \n  Tempo: {workout["time"]} \n  Localização: {workout["localization"]} \n  Clima: {workout["weather"]}")
 
 def update_workout(id):
-    for workout in workouts:
+    for workout in enumerate(workouts):
         if workout["id"] == id:
             print("1 - Atualizar Nome")
             print("2 - ATualizar Data")
@@ -56,30 +83,60 @@ def delete_workout(id):
         if workout["id"] == id:
             del workouts[i]
             print("Treino deletado com sucesso!")
+            save_workout_data()
             return
     print("Treino não encontrado!")
 
+def workouts_numerate():
+    for i, workout in enumerate(workouts):
+        print("Treinos: ")
+        print(f"{i + 1} - {workout["name"]}")
+
 while True:
+    read_workout_data()
+    print("-=" * 15)
     print("1 - Criar Treino")
     print("2 - Ver Treino")
     print("3 - Atualizar Treino")
     print("4 - Deletar Treino")
+    print("5 - Sair do Programa")
+    print("-=" * 15)
     
     resp = int(input(""))
     
     if resp == 1:
+        print("-=" * 15)
         name = input("Nome do treino: ")
         date = input("Data: ")
         distance = float(input("Distância percorrida em KM: "))
-        time = float(input("Tempo de treino: "))
+        time = float(input("Tempo de treino em minutos: "))
         localization = input("Localização: ")
         weather = input("Clima: ")
+        print("-=" * 15)
         
         create_workout(name, date, distance, time, localization, weather)
     
     if resp == 2:
+        print("-=" * 15)
         read_workout()
+        print("-=" * 15)
     
     if resp == 3:
-        id = int(input("ID do treino: "))
+        print("-=" * 15)
+        workouts_numerate()
+        id = int(input("Número do treino: "))
         update_workout(id)
+        print("-=" * 15)
+    
+    if resp == 4:
+        print("-=" * 15)
+        workouts_numerate()
+        id = int(input("ID do treino: "))
+        delete_workout(id)
+        print("-=" * 15)
+    
+    if resp == 5:
+        print("-=" * 15)
+        print("Saindo do programa...")
+        print("-=" * 15)
+        break
